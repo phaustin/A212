1e9c209e66a0ec1fa30bdf1e8122f579b4e66208
Phil Austin <paustin@eos.ubc.ca>
2016-03-22 09:05:29 -0700

    change name of utils directory

--- a212libs/linfit.py
+++ a212libs/linfit.py
@@ -2,6 +2,7 @@ import numpy as np
 from a212libs.utils import read_data
 import numpy.testing as ntest
 
+
 def LineFitWt(x, y, dy):
     """
     Use pine chapter 7 equations in section 7.3.2 to do
@@ -41,7 +42,7 @@ def LineFitWt(x, y, dy):
 
     """
     x = x.ravel()
-    y=y.ravel()
+    y = y.ravel()
     dy = dy.ravel()
     if len(x) != len(y):
         raise ValueError('x and y must have same number of elements')
@@ -50,24 +51,23 @@ def LineFitWt(x, y, dy):
     x = x.astype(np.float)
     y = y.astype(np.float)
     dy = dy.astype(np.float)
-    
     dy2 = dy**2.
-    denom= np.sum(1/(dy**2.))  # Pine 7.14
-    xnumerator = np.sum(x/dy2)
-    ynumerator = np.sum(y/dy2)
-    xhat = xnumerator/denom
-    yhat = ynumerator/denom
-    bnum = np.sum((x - xhat)*y/dy2)
-    bdenom = np.sum((x - xhat)*x/dy2)
-    slope = bnum/bdenom
-    yint = yhat - slope*xhat
+    denom = np.sum(1 / (dy**2.))  # Pine 7.14
+    xnumerator = np.sum(x / dy2)
+    ynumerator = np.sum(y / dy2)
+    xhat = xnumerator / denom
+    yhat = ynumerator / denom
+    bnum = np.sum((x - xhat) * y / dy2)
+    bdenom = np.sum((x - xhat) * x / dy2)
+    slope = bnum / bdenom
+    yint = yhat - slope * xhat
     #
     # add pine 7.16
     #
-    slope_uncertainty = 1./np.sum((x - xhat)*x/dy2)
+    slope_uncertainty = 1. / np.sum((x - xhat) * x / dy2)
     intercept_uncertainty = slope_uncertainty*\
           np.sum(x**2./dy2)/np.sum(1./dy2)
-    return slope,yint,slope_uncertainty,intercept_uncertainty
+    return slope, yint, slope_uncertainty, intercept_uncertainty
 
 
 def test_fit():
@@ -79,13 +79,16 @@ def test_fit():
     16              20               25
     23.21          -55               10
     """
+
     df_data = read_data(data_string)
     time = df_data['time'].values
     velocity = df_data['velocity'].values
     dy = df_data['uncertainty'].values
-    slope_w,yint_w, var_slope, var_inter = LineFitWt(time,velocity,dy)
-    ntest.assert_almost_equal([slope_w,yint_w,var_slope,var_inter],
-                              [-9.9656001,179.924646 ,0.5982539,140.7892905])
+    slope_w, yint_w, var_slope, var_inter = LineFitWt(time, velocity, dy)
+    ntest.assert_almost_equal(
+        [slope_w, yint_w, var_slope, var_inter],
+        [-9.9656001, 179.924646, 0.5982539, 140.7892905])
+
 
 if __name__ == "__main__":
     print('running linfit tests')
--- utils/h5_utils.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-def read_h5(h5filename,key):
-    with pd.HDFStore(h5filename,'r') as store:
-        out=store[key]
-    return out
-
